b75518fe0a5dcf8857b35b587d287b7f50a63ed2
vincenzozimb <vzimbardo9@gmail.com>
2021-04-24 18:47:04 +0200

    complex

--- src/differential_eq/numerov.c
+++ src/differential_eq/numerov.c
@@ -5,7 +5,7 @@ double complex numerov_step(double x, double dx,
                             complex double psi_curr, complex double psi_prec,
                             double F(double, void *), void *p){
 
-    double ris;
+    complex double ris;
     ris = (2.0 + 5.0 * dx * dx * F(x, p) / 6.0) * psi_curr;
     ris -= (1.0 - dx * dx * F(x - dx, p) / 12.0) * psi_prec;
     ris /= 1.0 - dx * dx * F(x + dx, p) / 12.0;
