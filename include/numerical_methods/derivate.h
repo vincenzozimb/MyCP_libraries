743996fd620fe4e2daf24797491b430b0fe23cf4
vincenzozimb <vzimbardo9@gmail.com>
2021-04-28 10:01:41 +0200

    derivative

--- include/numerical_methods/derivate.h
+++ include/numerical_methods/derivate.h
@@ -3,7 +3,7 @@
   @file     derivate_h
   @author   V. Zimbardo
   @date     Apr 2021
-  @brief    C routine for integration.               
+  @brief    C routines for numerical derivation.               
 */
 /*--------------------------------------------------------------------------*/
 
@@ -12,9 +12,23 @@
 
 /**
  * @brief Evaluate the first derivative of the function f in the point x0, using finite
+ * difference method of order O(dx^2). 
+ * 
+ * @param f function you want to calculate the first derivative
+ * @param x0 point where you want to calculate the derivative of the function f
+ * @param dx accuracy for finite difference method
+ * @param p pointer to the struct containing the parameters of f
+ *
+ * @return double
+ * The value of the derivative of f in the point x0, with an accuracy of order O(dx^2)
+ */
+double derivate(double f(double,void*), double x0, double dx, void *p);
+
+/**
+ * @brief Evaluate the first derivative of the function f in the point x0, using finite
  * difference method of order O(dx^4). 
  * 
- * @param f function you want to calculate the derivative
+ * @param f function you want to calculate the first derivative
  * @param x0 point where you want to calculate the derivative of the function f
  * @param dx accuracy for finite difference method
  * @param p pointer to the struct containing the parameters of f
@@ -22,6 +36,6 @@
  * @return double
  * The value of the derivative of f in the point x0, with an accuracy of order O(dx^4)
  */
-double derivate(double f(double,void*), double x0, double dx, void *p);
+double derivate_best(double f(double,void*), double x0, double dx, void *p);
 
 #endif
--- src/numerical_methods/derivate.c
+++ src/numerical_methods/derivate.c
@@ -3,5 +3,9 @@
 #include <numerical_methods/derivate.h>
 
 double derivate(double f(double,void*), double x0, double dx, void *p){
+    return ( f(x0+dx,p) - f(x0+dx,p) ) / (2*dx);
+}
+
+double derivate_best(double f(double,void*), double x0, double dx, void *p){
     return ( -f(x0+2*dx,p) + 8*f(x0+dx,p) -8*f(x0-dx,p) + f(x0-2*dx,p) ) / (12*dx);
 }
