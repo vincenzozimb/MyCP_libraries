72d60989da243d5ba5afb72368311a8fa9362005
vincenzozimb <vzimbardo9@gmail.com>
2021-04-28 09:39:22 +0200

    derivative

---
+++ include/numerical_methods/derivate.h
@@ -0,0 +1,27 @@
+/*-------------------------------------------------------------------------*/
+/**
+  @file     derivate_h
+  @author   V. Zimbardo
+  @date     Apr 2021
+  @brief    C routine for integration.               
+*/
+/*--------------------------------------------------------------------------*/
+
+#ifndef __DERIVATE_H__
+#define __DERIVATE_H__
+
+/**
+ * @brief Evaluate the first derivative of the function f in the point x0, using finite
+ * difference method of order O(dx^4). 
+ * 
+ * @param f function you want to calculate the derivative
+ * @param x0 point where you want to calculate the derivative of the function f
+ * @param dx accuracy for finite difference method
+ * @param p pointer to the struct containing the parameters of f
+ *
+ * @return double
+ * The value of the derivative of f in the point x0, with an accuracy of order O(dx^4)
+ */
+double derivate(double f(double,void*), double x0, double dx, void *p);
+
+#endif
---
+++ src/numerical_methods/derivate.c
@@ -0,0 +1,7 @@
+#include <assert.h>
+#include <math.h>
+#include <numerical_methods/derivate.h>
+
+double derivate(double f(double,void*), double x0, double dx, void *p){
+    return ( -f(x0+2*dx,p) + 8*f(x0+dx,p) -8*f(x0-dx,p) + f(x0-2*dx,p) ) / (12*dx);
+}
