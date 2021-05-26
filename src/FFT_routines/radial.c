f529db0f13f266c54df75cdda0fcd1627c031d9d
vincenzozimb <vzimbardo9@gmail.com>
2021-05-26 21:42:49 +0200

    normalization ok

--- src/FFT_routines/radial.c
+++ src/FFT_routines/radial.c
@@ -66,13 +66,12 @@ void radial_FFT(double r[], double f[], double L, double dr, int N, double k[], 
         sum += i * i * f[i]; 
     }
     sum *= 2 * L * L / (N * N);
-    sum /= 2 * M_PI * L / N;
     
     for(int i=0;i<N;i++){
         if( k[i] == 0.0){
             F[i] = sum;
         }else{
-            F[i] = - 2 * M_PI / k[i] * A2z_imag[i];
+            F[i] = - 4 * M_PI * M_PI / k[i] * A2z_imag[i];
         }
     }
 
@@ -136,7 +135,6 @@ void radial_IFFT(double k[], double F[], double L, double dk, int N, double r[],
         sum += i * i * F[i]; 
     }
     sum *= M_PI * M_PI / (L * L * N);
-    sum *= 2 * M_PI * L / N;
     
     for(int i=0;i<N;i++){
         if( r[i] == 0.0){
