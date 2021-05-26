d7110895441b0b876d6c896d1d8a715bfc671326
vincenzozimb <vzimbardo9@gmail.com>
2021-05-26 19:57:22 +0200

    radial IFFT and documentation

--- include/FFT_routines/radial.h
+++ include/FFT_routines/radial.h
@@ -1,7 +1,49 @@
+/*-------------------------------------------------------------------------*/
+/**
+  @file     radial_h
+  @author   V. Zimbardo
+  @date     May 2021
+  @brief    C routines for the radial fourier transform of a three dimensional
+            radial function and its inverse. The radial fourier transform is related
+            to the Hankel transform, so this code can excetute also the Hankel transform
+            of order nu = 1/2. The H_{1/2} transform and its inverse are nothing more 
+            than the sine and the inverse sine transforms.            
+*/
+/*--------------------------------------------------------------------------*/
+
 #ifndef __RADIAL_H__
 #define __RADIAL_H__
 
+/**
+ * @brief  Take a radial function and inizialize the radial fourier transform and the vector
+ *         contening the magnitude of frequencies.
+ * 
+ * @param r the radial coordinate vector
+ * @param f the radial function, calculated in the position of the vector r[]
+ * @param L the direct space length
+ * @param dr the radial discretization
+ * @param N the number of points. It must be a power of 2
+ * @param k the N-vector you want to inizialize with the magnitude of frequencies
+ * @param F the N-vector you want to inizialize with the radial transform 
+ * 
+ * @return void
+ */
 void radial_FFT(double r[], double f[], double L, double dr, int N, double k[], double F[]);
 
+/**
+ * @brief  Take a radial fourier transform and inizialize the radial fourier inverse 
+ *         transform and the vector contening the radial coordinate.
+ * 
+ * @param k the magnitude frequency vector
+ * @param F the radial fourier transform, calculated in the frequencies of the vector k[]
+ * @param L the direct space length
+ * @param dk the (radial) frequency discretization
+ * @param N the number of points. It must be a power of 2
+ * @param r the N-vector you want to inizialize with the radial coordinates
+ * @param f the N-vector you want to inizialize with the radial inverse transform 
+ * 
+ * @return void
+ */
+void radial_IFFT(double k[], double F[], double L, double dk, int N, double r[], double f[]);
 
 #endif
--- src/FFT_routines/radial.c
+++ src/FFT_routines/radial.c
@@ -9,7 +9,7 @@
 void radial_FFT(double r[], double f[], double L, double dr, int N, double k[], double F[]){
 
     /* check if N is a power of two */
-    // write the assertion, using the logarithm
+    // write the assertion
     
     /* initialize function A = r * f(r) */
     double A[N];
@@ -62,8 +62,90 @@ void radial_FFT(double r[], double f[], double L, double dr, int N, double k[], 
     }
 
     /* initialize the radial transform F */
+    double sum = 0.0;
     for(int i=0;i<N;i++){
-        F[i] = - 2 * M_PI / k[i] * A2z_imag[i];
+        sum += i * i * f[i]; 
+    }
+    sum *= 2 * L * L / (N * N);
+    
+    for(int i=0;i<N;i++){
+        if( k[i] == 0.0){
+            F[i] = sum;
+        }else{
+            F[i] = - 2 * M_PI / k[i] * A2z_imag[i];
+        }
+    }
+
+}
+
+void radial_IFFT(double k[], double F[], double L, double dk, int N, double r[], double f[]){
+
+    /* check if N is a power of two */
+    // write the assertion
+    
+    /* initialize function B = k * F(k) */
+    double B[N];
+    for(int i=0;i<N;i++){
+        B[i] = k[i] * F[i];
+    }
+
+    /* odd extensions */
+    double k2[2*N], B2[2*N];
+    for(int i=0;i<2*N;i++){
+        if(i < N){
+            k2[i] = k[i];
+            B2[i] = B[i];
+        }else if(N < i && i < 2*N){
+            k2[i] = -k[2*N-i];
+            B2[i] = -B[2*N-i];
+        }else{
+            k2[i] = 0.0;
+            B2[i] = 0.0;
+        }
+    }
+
+    /* complexification of B2 for gsl_fft_radix2_inverse */
+    int M = 2*N;
+    double B2z[2*M];
+    for(int i=0;i<M;i++){
+        B2z[2*i] = B2[i];
+        B2z[2*i+1] = 0.0;
+    }
+    
+    /* initialize radial coordinate vector */
+    double dr = L / N; 
+    double r2[M];
+    for(int i=0;i<M;i++){
+        r2[i] = dr * i;
+    }
+
+    /* inverse fourier transform of B2z */
+    gsl_fft_complex_radix2_inverse(B2z,1,M);
+
+    /* take the immaginary part */
+    double B2z_imag[M];
+    for(int i=0;i<M;i++){
+        B2z_imag[i] = B2z[2*i+1];
+    }
+
+    /* inizialize only the positive coordinates */
+    for(int i=0;i<N;i++){
+        r[i] = r2[i];
+    }
+
+    /* initialize the inverse radial transform f */
+    double sum = 0.0;
+    for(int i=0;i<N;i++){
+        sum += i * i * F[i]; 
+    }
+    sum *= M_PI * M_PI / (L * L * N);
+    
+    for(int i=0;i<N;i++){
+        if( r[i] == 0.0){
+            f[i] = sum;
+        }else{
+            f[i] = 1 / (4.0 * M_PI * M_PI * r[i]) * B2z_imag[i];
+        }
     }
 
 }
